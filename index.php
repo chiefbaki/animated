64a481cf31e56099ac6d3ab3d12b675b1757203f
IslamKurbanov <61545789+IslamKurbanov@users.noreply.github.com>
2021-03-25 12:25:54 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Animated</title>
+</head>
+<body>
+	<div class="hero">
+		<div class="navbar">
+			<img src="logo.png" alt="logo" class="logo">
+			<button type="button" class="button">SIgn up</button>
+		</div>
+		<div class="content">
+			<small>Welcome to our</small>
+			<h1>Worlds<br>Creative Studio</h1>
+			<button>Take a tour</button>
+		</div>
+		<div class="side-bar">
+			<img src="menu.png" alt="menu" class="menu">
+
+			<div class="social-links">
+				<img src="fb.png" alt="fb" class="fb">
+				<img src="ig.png" alt="ig" class="ig">
+				<img src="tw.png" alt="tw" class="tw">
+			</div>
+
+			<div class="useful-links">
+				<img src="info.png" alt="fb">
+				<img src="share.png" alt="ig">
+			</div>
+		</div>
+
+		<div class="bubbles">
+			<img src="bubble.png" alt="bubble">
+			<img src="bubble.png" alt="bubble">
+			<img src="bubble.png" alt="bubble">
+			<img src="bubble.png" alt="bubble">
+			<img src="bubble.png" alt="bubble">
+			<img src="bubble.png" alt="bubble">
+			<img src="bubble.png" alt="bubble">
+		</div>
+
+	</div>
+</body>
+</html>
